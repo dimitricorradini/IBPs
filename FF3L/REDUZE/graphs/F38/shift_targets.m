6bc2f4d1782b1e8f8139e96f1ba894b051597f1b
Dimitri Corradini <corradini@MacBookAir.box>
2025-11-14 13:11:17 +0100

    moved files

---
+++ FF3L/REDUZE/graphs/F38/shift_targets.m
@@ -0,0 +1,4 @@
+{
+{"F38", 5, 3680, MultiGraph[{Labeled[-1->3, "(-p2-p1,2*r;1)"], Labeled[-2->2, "(p1,0;2)"], Labeled[-3->1, "(p2,0;3)"], Labeled[2->3, "(p1-k2-k1,0;9)"], Labeled[1->3, "(p2+k3+k1,0;10)"], Labeled[4->4, "(k3+k2,0;13)"], Labeled[3->1, "(k3+k1,0;14)"], Labeled[2->3, "(k2+k1,0;15)"]}]},
+{"F38", 9, 3559, MultiGraph[{Labeled[-1->5, "(-p2-p1,2*r;1)"], Labeled[-2->7, "(p1,0;2)"], Labeled[-3->6, "(p2,0;3)"], Labeled[1->2, "(k1,0;4)"], Labeled[1->4, "(k2,0;5)"], Labeled[3->2, "(k3,0;6)"], Labeled[7->3, "(p1-k2-k1,0;9)"], Labeled[6->4, "(p2+k3+k1,0;10)"], Labeled[3->5, "(-k3+p1-k2-k1,0;11)"], Labeled[4->5, "(p2+k3+k2+k1,0;12)"], Labeled[2->6, "(k3+k1,0;14)"], Labeled[7->1, "(k2+k1,0;15)"]}]}
+}
