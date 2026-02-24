3a6f5a0503ce280f6f54cfe1320052f2e042e28f
Dimitri Corradini <64958902+dimitricorradini@users.noreply.github.com>
2026-02-24 17:46:14 +0100

    Delete FF3L directory

--- FF3L/REDUZE/graphs/F9/shift_targets.m
+++
@@ -1,4 +0,0 @@
-{
-{"F9", 8, 1470, MultiGraph[{Labeled[-1->5, "(-p2-p1,2*r;1)"], Labeled[-2->1, "(p1,0;2)"], Labeled[-3->1, "(p2,0;3)"], Labeled[2->4, "(k2,0;5)"], Labeled[3->6, "(k3,0;6)"], Labeled[1->2, "(-k1+p1,0;7)"], Labeled[1->6, "(k1+p2,0;8)"], Labeled[2->3, "(-k2-k1+p1,0;9)"], Labeled[3->5, "(-k2-k1-k3+p1,0;11)"], Labeled[4->5, "(k2+k1+p2+k3,0;12)"], Labeled[6->4, "(k1+p2+k3,0;14)"]}]},
-{"F9", 9, 1471, MultiGraph[{Labeled[-1->6, "(-p2-p1,2*r;1)"], Labeled[-2->1, "(p1,0;2)"], Labeled[-3->2, "(p2,0;3)"], Labeled[1->2, "(k1,0;4)"], Labeled[3->5, "(k2,0;5)"], Labeled[4->7, "(k3,0;6)"], Labeled[1->3, "(-k1+p1,0;7)"], Labeled[2->7, "(k1+p2,0;8)"], Labeled[3->4, "(-k2-k1+p1,0;9)"], Labeled[4->6, "(-k2-k1-k3+p1,0;11)"], Labeled[5->6, "(k2+k1+p2+k3,0;12)"], Labeled[7->5, "(k1+p2+k3,0;14)"]}]}
-}
